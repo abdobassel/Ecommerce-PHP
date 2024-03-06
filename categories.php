5717c982b0fb16f467a83099e5ede4cd5c3bf28a
abdobassel <abdobassel@gmail.com>
2024-03-06 22:50:23 +0200

    try images grid row

--- categories.php
+++ categories.php
@@ -5,22 +5,44 @@ include "init.php";
 
 $cat_id = $_GET['catid'];
 $pagename = str_replace('-', ' ', $_GET['pagename']);
-$itmes = getItems($cat_id); ?>
+$items = getItems($cat_id); ?>
 
 <div class="container">
     <h1 class="text-center"><?php
                             echo $pagename;
                             ?>
     </h1>
-    <?php
-    foreach ($itmes as $item) { ?>
-        <ul>
-            <li><?php echo
-                $item['Name'];
-                ?></li>
+    <div class="row">
+        <?php
+        if (empty($items)) {
+            echo '<div class="alert alert-danger"><h3 class=" text-center">There is No items yet...</h3> </div>' . '<br>';
+        } else {
+            foreach ($items as $item) { ?>
+                <div class="col-sm-6 col-md-3">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="1.jpg" alt="">
+                        <div class="caption">
+                            <h3><?php echo
+                                $item['Name'];
+                                ?></h3>
+                            <p><?php
+                                echo  $item['Description'];
+                                ?></p>
+                            <h4><?php
+                                echo  $item['Price'];
+                                ?></h4>
+                        </div>
+                    </div>
 
-        </ul>
-    <?php  }
-    ?>
+                </div>
 
-    <?php include $tpl . "footer.php";
+
+
+        <?php  }
+        }
+
+        ?>
+    </div>
+</div>
+
+<?php include $tpl . "footer.php";
