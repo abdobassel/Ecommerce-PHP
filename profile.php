b2532566e4154b6c0b4d1bf3e6187dd49f29d993
abdobassel <abdobassel@gmail.com>
2024-03-07 23:00:55 +0200

    Profile fetch Comments for one user

--- profile.php
+++ profile.php
@@ -27,22 +27,61 @@ if (isset($_SESSION['user'])) {
     <div class="information-block">
         <div class="container">
             <div class="panel panel-primary">
-                <div class="panel-heading">My Ads</div>
+                <div class="panel-heading">My Ads اعلاناتي</div>
                 <?php
-                $stmt2 = $con->prepare("SELECT * FROM items WHERE Mem_ID = ?");
+                $stmt2 = $con->prepare("SELECT * FROM items WHERE Mem_ID = ? ORDER BY Item_Id DESC");
                 $stmt2->execute(array($userInfo['UserID']));
 
                 $items = $stmt2->fetchAll();
-                if (!empty($items)) {
-                    foreach ($items as $item) {
+
+
+
                 ?>
-                        <div class="panel-body"> Item : <?php echo $item['Name'] . ' In [ ' . $item['Add_Date'] . ' ]' ?> </div>
-                    <?php }
-                } else { ?>
-                    <div class="panel-body"> There is no Items Here Now... Add Items if you like.. </div>
+                <div class="panel-body">
+
+                    <div class="row">
+                        <?php
+                        if (empty($items)) {
+                            echo '<div class="alert alert-danger"><h3 class="text-center">There is No items yet...</h3> </div>' . '<br>';
+                        } else {
+                            foreach ($items as $item) { ?>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail item-box">
+                                        <span class="price-tag"><?php
+                                                                echo  $item['Price'];
+                                                                ?></span>
+                                        <img class="img-responsive" src="1.jpg" alt="">
+                                        <div class="caption">
+                                            <h3><?php echo
+                                                $item['Name'];
+                                                ?></h3>
+                                            <p><?php
+                                                echo  $item['Description'];
+                                                ?></p>
+                                            <p> Date : <?php
+                                                        echo  $item['Add_Date'];
+                                                        ?></p>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+
+                        <?php  }
+                        }
+
+                        ?>
+                    </div>
+
+
+                </div>
+
+
                 <?php
 
-                }
+
 
                 ?>
 
@@ -55,9 +94,55 @@ if (isset($_SESSION['user'])) {
         <div class="container">
             <div class="panel panel-primary">
                 <div class="panel-heading">My Comments</div>
-                <div class="panel-body">Name : <?php if (isset($_SESSION['user'])) {
-                                                    echo $_SESSION['user'];
-                                                } ?> </div>
+                <?php
+                $stmt3 = $con->prepare("SELECT body,date FROM comments WHERE user_id = ? ORDER BY comment_id DESC");
+                $stmt3->execute(array($userInfo['UserID']));
+
+                $comments = $stmt3->fetchAll();
+
+
+
+                ?>
+                <div class="panel-body">
+                    <div class="row">
+
+
+
+                        <?php
+                        if (empty($comments)) {
+                            echo '<div class="alert alert-danger"><h3 class="text-center">There is No Comments yet...</h3> </div>' . '<br>';
+                        } else {
+                            foreach ($comments as $comment) { ?>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail item-box">
+
+
+                                        <div class="caption">
+                                            <h3><?php echo
+                                                $comment['body'];
+                                                ?></h3>
+                                            <p><?php
+                                                echo  $comment['date'];
+                                                ?></p>
+
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+
+                        <?php  }
+                        }
+
+                        ?>
+                    </div>
+
+
+                </div>
+
+
             </div>
         </div>
     </div>
