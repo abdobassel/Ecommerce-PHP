e7f53501052bf8ea9eed686655315779331cb323
abdobassel <abdobassel@gmail.com>
2024-03-08 00:55:17 +0200

    Register user Success done with validate

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
+ob_start();
 session_start();
 $pageTitle = 'Login';
+
 if (isset($_SESSION['user']) || isset($_SESSION['Username'])) {
     header("Location: index.php");
 }
@@ -8,31 +10,85 @@ include "init.php";
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $hashedPass = sha1($password);
+    if (isset($_POST['login'])) {
+
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $hashedPass = sha1($password);
 
 
 
-    //  echo 'Welcome ' . $username . " hash pass " . $hashedPass."<br>";
-    //$_SESSION['Username'] = $username; // مؤقتا حتى تشغيل ال mysql لاانه فيه مشكلة ومفيش نت لحلها
 
-    // Admin Login
-    //check user if in database or not
-    // con => in config file
-    // استعلام عن طريق prepare عن المستخدم 
-    // select = usernme and password from db => users table => for check it
-    // for admins only =>> = 1 means he is admin ;
-    $stmt = $con->prepare("SELECT Username, Password FROM users WHERE Username = ? AND Password = ?");
-    $stmt->execute(array($username, $hashedPass)); // search in db
-    $count = $stmt->rowCount();
+        $stmt = $con->prepare("SELECT Username, Password FROM users WHERE Username = ? AND Password = ?");
+        $stmt->execute(array($username, $hashedPass)); // search in db
+        $count = $stmt->rowCount();
 
-    if ($count > 0) {
-        $_SESSION['user'] = $username;
+        if ($count > 0) {
+            $_SESSION['user'] = $username;
 
 
-        header("Location: index.php");
-        exit();
+            header("Location: index.php");
+            exit();
+        }
+    } else { //signup code
+        $formErrors = array();
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        $email = $_POST['email'];
+
+
+
+        if (isset($_POST['username'])) {
+            //$users = $_POST['username']; // without filter =>no securety
+            //echo $users;
+            $filterUsername = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS); //security
+            echo $filterUsername;
+        } else {
+            $formErrors[] = 'Username Not Exists';
+        }
+
+
+        if (isset($_POST['email'])) {
+
+            $filterEmail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); //security
+            if (!filter_var($filterEmail, FILTER_VALIDATE_EMAIL)) {
+                $formErrors[] = '<div class="alert alert-danger">Email Is Not Valid </div>';
+            }
+        } else {
+            $formErrors[] = 'Username Not Exists';
+        }
+
+        if (isset($_POST['password']) && isset($_POST['password2'])) {
+            if (empty($_POST['password'])) {
+                $formErrors[] = '<div class="alert alert-danger">Password IS Empty Don\'t Do That </div>';
+            }
+            $pass1 = sha1($_POST['password']);
+            $pass2 = sha1($_POST['password2']);
+            if ($pass1 !== $pass2) {
+                $formErrors[] = '<div class="alert alert-danger">Password Doesn\'t match </div>';
+            } else {
+            }
+        } else {
+            $formErrors[] = '<div class="alert alert-danger">Password Is Empty</div>';
+        }
+
+        if (empty($formErrors)) {
+            $check = checkItem('Username', 'users', $username);
+            if ($check > 0) {
+                $formErrors[] = '<div class="alert alert-danger">User Is Exists Change The Username Please...</div>';
+            } else {
+                $stmt = $con->prepare("INSERT INTO users(Username,Password,Email, RegStatus,Date)
+                VALUES(:zuser,:zpass,:zmail ,0,now())
+                ");
+                $stmt->execute(array('zuser' => $username, 'zpass' => sha1($password), 'zmail' => $email));
+                $count = $stmt->rowCount();
+                if ($count > 0) {
+                    echo '<div class="alert alert-success">Success Registerd Welcome </div>' . '<br>';
+                }
+            }
+        }
     }
 }
 ?>
@@ -42,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <input class="form-control" type="text" name="username" placeholder="UserName" autocomplete="off">
         <input class="form-control" type="password" name="password" placeholder="password" autocomplete="new-password">
-        <input class="btn btn-primary btn-block" type="submit" value="login">
+        <input class="btn btn-primary btn-block" name="login" type="submit" value="login">
 
     </form>
 
@@ -53,9 +109,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input class="form-control" type="email" name="email" placeholder="Email" autocomplete="off">
         <input class="form-control" type="password" name="password" placeholder="password" autocomplete="new-password">
         <input class="form-control" type="password" name="password2" placeholder="type password again" autocomplete="new-password">
-        <input class="btn btn-success btn-block" type="submit" value="signup">
+        <input class="btn btn-success btn-block" name="signup" type="submit" value="signup">
 
     </form>
+    <div class="text-center the-errors">
+        <?php
+        if (!empty($formErrors)) {
+            foreach ($formErrors as  $e) {
+                echo $e . "<br>";
+            }
+        }
+        ?>
+    </div>
 </div>
 <?php
 include $tpl . "footer.php";
+ob_end_flush();
+?>
